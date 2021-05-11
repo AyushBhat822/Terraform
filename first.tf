8b98dc39f385118774cbf8d420dfd88749f770b8
Ayush Bhat <67255324+AyushBhat822@users.noreply.github.com>
2021-05-11 18:21:08 +0530

    Update first.tf

--- first.tf
+++ first.tf
@@ -4,19 +4,3 @@ region = "ap-south-1"
 profile = "default"
 
 }
-
-resource "aws_instance"  "os1"  {
-
-ami = "ami-010aff33ed5991201"
-instance_type = "t2.micro"
-tags = { 
-    Name = "my first TF os"
-     }
-
-}
-
-
-
-
-
- 
